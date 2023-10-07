1fc9381f4d806897d8ce608ac266300562a34244
JackMeds <1530043661@qq.com>
2023-10-07 13:46:15 +0800

    完善注册代码

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,4 +1,5 @@
-<template lang="">
+<template>
+  <authNav class="authNav" />
   <!-- 中央输入框 -->
   <div class="centerInput">
     <div class="title">
@@ -6,23 +7,33 @@
     </div>
     <div class="inputGroup">
       <div class="username">
-        <div class="inputTitle"><p>用户名</p></div>
+        <div class="inputTitle">
+          <p>用户名</p>
+        </div>
         <n-input type="text" placeholder="请输入用户名" clearable />
       </div>
       <div class="password">
-        <div class="inputTitle"><p>密码</p></div>
+        <div class="inputTitle">
+          <p>密码</p>
+        </div>
         <n-input type="password" show-password-on="mousedown" placeholder="请输入密码" clearable />
       </div>
     </div>
     <div class="buttonGroup">
-        <n-button strong ghost type="primary">注册</n-button>
-        <n-button type="primary">登录</n-button>
+      <n-button strong ghost type="primary" @click="toRegister">没有账户，前去注册</n-button>
+      <n-button type="primary">登录</n-button>
     </div>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router"; // 引入useRouter
+import authNav from "../../components/auth/authNav.vue"
+
+const router = useRouter();
+
+const toRegister = () => {
+  router.push("/auth/register");
+}
 </script>
-<style>
-@import "../../style/auth/login.css";
-</style>
+<style>@import "../../style/auth/login.css";</style>
--- src/views/auth/register.vue
+++ src/views/auth/register.vue
@@ -1,4 +1,5 @@
-<template lang="">
+<template>
+  <authNav class="authNav" />
   <!-- 中央输入框 -->
   <div class="centerInput">
     <div class="title">
@@ -6,53 +7,65 @@
     </div>
     <div class="inputGroup">
       <div class="username">
-        <div class="inputTitle"><p>用户名</p></div>
+        <label class="inputTitle">
+          <p>用户名</p>
+        </label>
         <n-input type="text" placeholder="请输入用户名" clearable />
-        
+
       </div>
       <div class="fristPassword">
-        <div class="inputTitle"><p>密码</p></div>
-        <n-input
-          type="password"
-          show-password-on="mousedown"
-          placeholder="请输入密码"
-          clearable
-        />
+        <div class="inputTitle">
+          <p>密码</p>
+        </div>
+        <n-input type="password" show-password-on="mousedown" placeholder="请输入密码" clearable />
       </div>
       <div class="secPassword">
-        <div class="inputTitle"><p>确认密码</p></div>
-        <n-input
-          type="password"
-          show-password-on="mousedown"
-          placeholder="请输入密码"
-          clearable
-        />
+        <div class="inputTitle">
+          <p>确认密码</p>
+        </div>
+        <n-input type="password" show-password-on="mousedown" placeholder="请输入密码" clearable />
       </div>
       <div class="email">
-        <div class="inputTitle"><p>邮箱</p></div>
+        <div class="inputTitle">
+          <p>邮箱</p>
+        </div>
         <n-input type="text" placeholder="请输入邮箱" clearable />
       </div>
       <div class="emailCode">
-        <div class="inputTitle"><p>邮箱验证码</p></div>
+        <div class="inputTitle">
+          <p>邮箱验证码</p>
+        </div>
         <n-input-group>
           <n-input type="text" placeholder="请输入邮箱验证码" clearable />
           <n-button type="primary" ghost> 发送验证码 </n-button>
         </n-input-group>
       </div>
-      <div class="elua"></div>
+      <div class="registerEula">
+        <n-checkbox class="checkbox" v-model:checked="eulaCheck" />
+        <n-checkbox class="checkbox" v-model:checked="eulaCheck" />
+        <span>已阅读并同意</span><span class="eula">Notesharea用户协议</span><span>和</span><span class="eula">Notesharea隐私政策</span>
+      </div>
     </div>
     <div class="buttonGroup">
-      <n-button strong ghost type="primary">注册</n-button>
-      <n-button type="primary">登录</n-button>
+      <n-button strong ghost type="primary" @click="toLogin">已有账户，去登录</n-button>
+      <n-button type="primary">注册</n-button>
     </div>
-    
+
   </div>
-  
 </template>
 <script setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router"; // 引入useRouter
+import authNav from "../../components/auth/authNav.vue"
 
+const router = useRouter();
+
+let eulaCheck = ref(false);
+
+const toLogin = () => {
+  router.push("/auth/login");
+}
 </script>
-<style>
+<style scoped>
 @import "../../style/auth/register.css";
 </style>
